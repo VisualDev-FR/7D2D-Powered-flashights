50be7fd5c3cc436b974e308cf4dd705e5969473e
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-10-26 14:27:20 +0200

    initial commit

---
+++ Scripts/MinEventActionDecayGunLight.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MinEventActionDecayGunLight : MinEventActionDecayLightAbstract
+{
+    public override ItemValue GetLightItemValue(MinEventParams _params)
+    {
+        return null;
+    }
+
+    public override Transform GetLightTransform(MinEventParams _params)
+    {
+        return null;
+    }
+}
