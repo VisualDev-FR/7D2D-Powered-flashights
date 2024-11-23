c794f26eed238cfb3072f6c2c0e744d99d85cff0
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-11-23 12:26:19 +0100

    balancing flashlights decaying

--- Scripts/MinEventActions/MinEventActionDecayLightAbstract.cs
+++ Scripts/MinEventActions/MinEventActionDecayLightAbstract.cs
@@ -6,7 +6,15 @@ public abstract class MinEventActionDecayLightAbstract : MinEventActionTargetedB
 
     // values in seconds, which say when the sparkle effect must be played
     // follows a cubic curve, to have more frequent sparkles when the light has a low battery
-    public static readonly HashSet<float> sparkleTimes = new HashSet<float>() { 0, 13, 25, 41, 61, 85, 113, 145, 181, 221, 265, 313, 365, 421, 481, 545, 613, 685, 761, 841, 925, 1013, 1105, 1201, 1301, 1405, 1513, 1625, 1741, 1861 };
+    public static readonly HashSet<float> sparkleTimes = new HashSet<float>() {
+        0, 13, 25, 41, 61, 85, 113, 145, 181, 221,
+        265, 313, 365, 421, 481, 545, 613, 685, 761,
+        841, 925, 1013, 1105, 1201, 1301, 1405, 1513,
+        1625, 1741, 1861, 1985, 2113, 2245, 2381, 2521,
+        2665, 2813, 2965, 3121, 3281, 3445, 3613, 3785,
+        3961, 4141, 4325, 4513, 4705, 4901, 5101, 5305,
+        5513, 5725, 5941, 6161, 6385, 6613, 6845, 7081
+     };
 
     public abstract PoweredLightAbstract PoweredLightSource { get; }
 
