99243a90827162617629489b55ddc06b78ce2d49
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2025-02-20 16:26:21 +0100

    update project to support mod-utils.dll

--- Scripts/Config.cs
+++ Scripts/Config.cs
@@ -2,7 +2,7 @@ public class CaveLightConfig
 {
     private static readonly ModConfig config = new ModConfig("CaveLights");
 
-    public static float moonLightScale = config.GetPropertyFloat("moonLightScale");
+    public static float moonLightScale = config.GetFloat("moonLightScale");
 
-    public static float sunIntensityScale = config.GetPropertyFloat("sunIntensityScale");
+    public static float sunIntensityScale = config.GetFloat("sunIntensityScale");
 }
